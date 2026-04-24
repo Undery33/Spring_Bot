c995b34260bd7c61461c55011ec17d67d5c1b499
Undery33 <xodml1122@naver.com>
2026-04-24 14:40:08 +0900

    Create Base Code

---
+++ src/main/java/com/undery/dersoon_discord/DersoonDiscordApplication.java
@@ -0,0 +1,13 @@
+package com.undery.dersoon_discord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DersoonDiscordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DersoonDiscordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/undery/dersoon_discord/DersoonDiscordApplicationTests.java
@@ -0,0 +1,14 @@
+package com.undery.dersoon_discord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DersoonDiscordApplicationTests {
+
+    @Test
+    void contextLoads() {
+        
+    }
+
+}
